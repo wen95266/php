5f418e154279823b5081d330bdd4c9db30e4d55a
wen952555 <wenxiu9525@outlook.com>
2025-05-30 17:31:30 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new mysqli("mysql12.serv00.com", "m10300_yh", "Wenxiu1234*", "m10300_sj");
-if ($db->connect_errno) {
-    die("数据库连接失败: " . $db->connect_error);
-}
-$db->set_charset("utf8mb4");
-session_start();
-?>
