f9a1a9cfa66d090d5df32d1edd330cf945dba6d1
wen952555 <wenxiu9525@outlook.com>
2025-05-30 17:32:12 +0800

    Delete score_history.php

--- score_history.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-if (!isset($_SESSION['uid'])) die(json_encode(['error'=>'未登录']));
-$uid = $_SESSION['uid'];
-$res = $db->query("SELECT change_val, after_val, change_type, remark, create_time FROM score_history WHERE user_id=$uid ORDER BY id DESC LIMIT 50");
-$list = [];
-while ($row = $res->fetch_assoc()) $list[] = $row;
-echo json_encode($list);
-?>
