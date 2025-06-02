8fbc4728982f2d5cbc74843c1aafb4aa41eddebb
wen952555 <wenxiu9525@outlook.com>
2025-06-02 12:32:20 +0800

    Create db.config.js

---
+++ server/config/db.config.js
@@ -0,0 +1,16 @@
+// server/config/db.config.js
+require('dotenv').config();
+
+module.exports = {
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    DB: process.env.DB_NAME || "thirteen_water_db",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
