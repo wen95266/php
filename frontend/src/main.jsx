9623d9aa3c7f3c76111aaa51249421f04eda9749
wen952555 <wenxiu9525@outlook.com>
2025-06-02 23:40:38 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // 引入全局样式
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
