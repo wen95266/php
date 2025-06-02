d67cf38dde5a1628444f40dab2c35bb0b55bc05a
wen952555 <wenxiu9525@outlook.com>
2025-06-02 14:05:17 +0800

    Delete server directory

--- server/config/auth.config.js
+++
@@ -1,8 +0,0 @@
-// server/config/auth.config.js
-require('dotenv').config();
-
-module.exports = {
-    secret: process.env.JWT_SECRET || "fallback-super-secret-key",
-    jwtExpiration: 3600,          // 1 hour in seconds
-    // jwtRefreshExpiration: 86400, // 24 hours for refresh token (if implemented)
-};
--- server/middleware/index.js
+++
@@ -1,8 +0,0 @@
-// server/middleware/index.js
-const authJwt = require("./authJwt.middleware.js");
-const verifySignUp = require("./verifySignUp.middleware.js");
-
-module.exports = {
-  authJwt,
-  verifySignUp
-};
