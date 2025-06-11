6e27ec2951624157577d9cac883dd80fcd0531e1
wen952555 <wenxiu9525@outlook.com>
2025-06-11 22:30:45 +0800

    Update AIControl.js

--- frontend/src/components/AIControl.js
+++ frontend/src/components/AIControl.js
@@ -17,17 +17,7 @@ const AIControl = ({ onAIDivide, onReset, gameStatus }) => {
       >
         重新开始
       </button>
-      
-      <div className="game-rules">
-        <h3>游戏规则:</h3>
-        <ul>
-          <li>头道: 3张牌</li>
-          <li>中道: 5张牌</li>
-          <li>尾道: 5张牌</li>
-          <li>尾道 &gt; 中道 &gt; 头道</li>
-          <li>拖拽扑克牌到相应区域</li>
-        </ul>
-      </div>
+      {/* 游戏规则区块已移除 */}
     </div>
   );
 };
