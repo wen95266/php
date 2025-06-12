74532f9175cd364d5aaacf9d95f2468a88e795c0
wen952555 <wenxiu9525@outlook.com>
2025-06-12 13:13:52 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Lobby from './pages/Lobby';
 import GameRoom from './pages/GameRoom';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Lobby />} />
-        <Route path="/room/:roomId" element={<GameRoom />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <GameRoom />;
 }
